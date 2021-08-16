3591a75f63bfa8f1a74ae61d3f035145389d0182
Mariana Moreira <m.camargomoreira@gmail.com>
2021-08-16 15:52:12 +0200

    simple react boiler plate setup

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import ReactDOM from "react-dom";
+
+
+const App = () => {
+  return <div>hello worlds</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
